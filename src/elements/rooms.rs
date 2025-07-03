15cf39ad64542929c3f5e25b6f617a45552511cc
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-07-03 19:31:03 +0200

    Randomize room order

--- src/elements/rooms.rs
+++ src/elements/rooms.rs
@@ -7,6 +7,7 @@
 
 
 use std::collections::HashSet;
+use rand::prelude::*;
 
 use crate::elements::ressources::*;
 
@@ -17,7 +18,8 @@ use crate::elements::ressources::*;
 pub struct Room {
     room_type: RoomType,
     repair_field: [(RessourceType, bool); 3], // RessourceType and amount needed to repair
-    divert_field: [(RessourceType, bool); 3], 
+    divert_field: [RessourceType; 3],
+    diverted: bool, 
     ability: String,
     version: Version,
 }
@@ -45,7 +47,7 @@ impl Room {
     fn new(
         room_type: RoomType,
         repair_field: [(RessourceType, bool); 3],
-        divert_field: [(RessourceType, bool); 3],
+        divert_field: [RessourceType; 3],
         ability: String,
         version: Version,
     ) -> Self {
@@ -55,6 +57,7 @@ impl Room {
             divert_field,
             ability,
             version,
+            diverted: false,
         }
     }
             
@@ -74,9 +77,9 @@ impl Room {
                                 (RessourceType::Metal, false),
                             ],
                             [
-                                (RessourceType::Metal, false),
-                                (RessourceType::Metal, false),
-                                (RessourceType::Nanobots, false),
+                                RessourceType::Metal,
+                                RessourceType::Metal,
+                                RessourceType::Nanobots,
                             ],
                         
                         "Move a player's meeple to a room that has another meeple in it"
@@ -95,9 +98,9 @@ impl Room {
                                 (RessourceType::Data, false),
                             ],
                         [
-                                (RessourceType::Energy, false),
-                                (RessourceType::Metal, false),
-                                (RessourceType::Nanobots, false),
+                                RessourceType::Energy,
+                                RessourceType::Metal,
+                                RessourceType::Nanobots,
                             ],                       
                         "Move a player's meeple to a room that has another meeple in it"
                                     .to_string(),
@@ -114,9 +117,9 @@ impl Room {
                                 (RessourceType::Metal, false),
                             ],
                         [
-                                (RessourceType::Metal, false),
-                                (RessourceType::Energy, false),
-                                (RessourceType::Nanobots, false),
+                                RessourceType::Metal,
+                                RessourceType::Energy,
+                                RessourceType::Nanobots,
                             ],                       
                         "Place 2 protection tokens on any room[s] [this ends at the start of your next turn]"
                                     .to_string(),
@@ -132,9 +135,9 @@ impl Room {
                                 (RessourceType::Energy, false),
                             ],
                         [
-                                (RessourceType::Data, false),
-                                (RessourceType::Energy, false),
-                                (RessourceType::Energy, false),
+                                RessourceType::Data,
+                                RessourceType::Energy,
+                                RessourceType::Energy,
                             ],                       
                         "As two actions, give three action tokens to any other player[s]"
                                     .to_string(),
@@ -151,9 +154,9 @@ impl Room {
                                 (RessourceType::Data, false),
                             ],
                         [
-                                (RessourceType::Data, false),
-                                (RessourceType::Metal, false),
-                                (RessourceType::Nanobots, false),
+                                RessourceType::Data,
+                                RessourceType::Metal,
+                                RessourceType::Nanobots,
                             ],                       
                         "Look at the next 5 resource cards. Then put them back in any order."
                                     .to_string(),
@@ -170,9 +173,9 @@ impl Room {
                                 (RessourceType::Nanobots, false),
                             ],
                         [
-                                (RessourceType::Data, false),
-                                (RessourceType::Energy, false),
-                                (RessourceType::Nanobots, false),
+                                RessourceType::Data,
+                                RessourceType::Energy,
+                                RessourceType::Nanobots,
                             ],                       
                         "Repair a damaged room by one space on the Repair Track. Discard the matching card."
                                     .to_string(),
@@ -189,9 +192,9 @@ impl Room {
                                 (RessourceType::Data, false),
                             ],
                         [
-                                (RessourceType::Data, false),
-                                (RessourceType::Data, false),
-                                (RessourceType::Energy, false),
+                                RessourceType::Data,
+                                RessourceType::Data,
+                                RessourceType::Energy,
                             ],                       
                         "Give, take, or exchange a resource card with another player."
                                     .to_string(),
@@ -208,9 +211,9 @@ impl Room {
                                 (RessourceType::Energy, false),
                             ],
                         [
-                                (RessourceType::Data, false),
-                                (RessourceType::Metal, false),
-                                (RessourceType::Energy, false),
+                                RessourceType::Data,
+                                RessourceType::Metal,
+                                RessourceType::Energy,
                             ],                       
                         "Look at the next 3 Damage cards and put them back in any order."
                                     .to_string(),
@@ -226,9 +229,9 @@ impl Room {
                                 (RessourceType::Universal, false),
                             ],
                         [
-                                (RessourceType::Universal, false),
-                                (RessourceType::Universal, false),
-                                (RessourceType::Universal, false),
+                                RessourceType::Universal,
+                                RessourceType::Universal,
+                                RessourceType::Universal,
                             ],                       
                         "When all rooms have diverted power, get here and use 1 action to reactivate the Energy Core."
                                     .to_string(),
@@ -247,9 +250,9 @@ impl Room {
                                 (RessourceType::Universal, false),
                             ],
                         [
-                                (RessourceType::Universal, false),
-                                (RessourceType::Universal, false),
-                                (RessourceType::Universal, false),
+                                RessourceType::Universal,
+                                RessourceType::Universal,
+                                RessourceType::Universal,
                             ],                       
                         "When all rooms have diverted power, get here and use 1 action to reactivate the Energy Core."
                                     .to_string(),
@@ -259,6 +262,9 @@ impl Room {
                 );
             }
         }
+        // Shuffle the rooms
+        let mut rng = rand::rng();
+        rooms.shuffle(&mut rng);
     rooms
     }
 
@@ -271,7 +277,7 @@ mod tests {
 
     #[test]
     fn test_init_rooms() {
-        let rooms = Room::init_rooms(Version::V1);
+        let rooms = dbg!(Room::init_rooms(Version::V1));
         assert_eq!(rooms.len(), 9, "Expected 9 rooms");
         let room_types: HashSet<RoomType> = rooms.iter().map(|r| r.room_type.clone()).collect();
         assert!(room_types.contains(&RoomType::CrewQuarters));
@@ -282,6 +288,6 @@ mod tests {
         assert!(room_types.contains(&RoomType::RepairCenter));
         assert!(room_types.contains(&RoomType::MessHall));
         assert!(room_types.contains(&RoomType::Bridge));
-        assert!(room_types.contains(&RoomType::EnergyCore));        
+        assert!(room_types.contains(&RoomType::EnergyCore));      
     }
 }
