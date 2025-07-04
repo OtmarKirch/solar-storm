22d9f2096ebbfc4a09a29f710dbc6ad4485fe883
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-07-04 11:20:17 +0200

    Refactor ressources

--- src/elements/ressources.rs
+++ src/elements/ressources.rs
@@ -1,3 +1,5 @@
+use crate::game::*;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum RessourceType {
     Metal,
@@ -17,13 +19,21 @@ impl Ressource {
         Self { ressource_type }
     }
 
-    pub fn init(universal_cnt: u32) -> Vec<Self> {
+    pub fn init(difficulty: Difficulty) -> Vec<Self> {
         let mut ressources: Vec<Ressource> = vec![];
         let metal_vec: Vec<Ressource> = vec![Ressource::new(RessourceType::Metal); 15];
         let energy_vec: Vec<Ressource> = vec![Ressource::new(RessourceType::Energy); 15];
         let nanobots_vec: Vec<Ressource> = vec![Ressource::new(RessourceType::Nanobots); 15];
         let data_vec: Vec<Ressource> = vec![Ressource::new(RessourceType::Data); 15];
-        let universal_vec: Vec<Ressource> = vec![Ressource::new(RessourceType::Universal); universal_cnt as usize];
+
+        let universal_cnt: usize = match difficulty {
+            Difficulty::Easy => 8,
+            Difficulty::Medium => 6,
+            Difficulty::Hard => 4,
+            Difficulty::Veteran => 2,
+            Difficulty::Realist => 0,
+        };
+        let universal_vec: Vec<Ressource> = vec![Ressource::new(RessourceType::Universal); universal_cnt];
 
         ressources.extend(metal_vec);
         ressources.extend(energy_vec);
@@ -41,7 +51,7 @@ mod tests {
 
     #[test]
     fn test_ressource_initialization() {
-        let ressources = Ressource::init(2);
+        let ressources = Ressource::init(Difficulty::Veteran);
         assert_eq!(ressources.len(), 62); // 15 Metal + 15 Energy + 15 Nanobots + 15 Data + 2 Universal
         assert_eq!(ressources.iter().filter(|r_t| r_t.ressource_type == RessourceType::Metal).count(), 15);
         assert_eq!(ressources.iter().filter(|r_t| r_t.ressource_type == RessourceType::Energy).count(), 15);
